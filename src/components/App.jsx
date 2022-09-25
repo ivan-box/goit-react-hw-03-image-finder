3454bff43710173d0ea3ac44861728e00872d9db
ivan-box <iboks77@gmail.com>
2022-09-26 00:10:03 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 export const App = () => {
   return (
     <div
@@ -10,12 +11,17 @@ export const App = () => {
         color: '#010101',
       }}
     >
+      {/* axios.defoults.baseURL='https://pixabay.com/api/?key=29479728-a98d2355de22f92bb93dea3e0&q=yellow+flowers&image_type=photo&pretty=true' */}
       {/* https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12 */}
       {/* id - унікальний ідентифікатор
       webformatURL - посилання на маленьке
       зображення для списку карток
       largeImageURL - посилання на велике
       зображення для модального вікна React homework template */}
+      <Searchbar />
+      <ImageGalleryItem />
+      <ImageGallery />
+      <Modal />
     </div>
   );
 };
